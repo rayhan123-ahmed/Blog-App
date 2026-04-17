451102f8c60b8f93f42f70dbc3268ef562fd26a3
Rayhan Ahmed <rmd09181@gmail.com>
2026-04-18 05:16:48 +0600

    initialize script.js with element selectors for title, content, and posts

---
+++ script.js
@@ -0,0 +1,5 @@
+// select all elements
+const titleInput = document.querySelector("#titleInput");
+const contentInput = document.querySelector("#contentInput");
+const addPostBtn = document.querySelector("#addPostBtn");
+const postsContainer = document.querySelector("#postsContainer");

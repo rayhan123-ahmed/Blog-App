a828bc9ca163a5bbe5b1541807e87ab636fb8c5b
Rayhan Ahmed <rmd09181@gmail.com>
2026-04-18 05:31:27 +0600

    implement post creation functionality

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ const titleInput = document.querySelector("#titleInput");
 const contentInput = document.querySelector("#contentInput");
 const addPostBtn = document.querySelector("#addPostBtn");
 const postsContainer = document.querySelector("#postsContainer");
+
+addPostBtn.addEventListener("click", () => {
+  const title = titleInput.value;
+  const content = contentInput.value;
+
+  if (title === "" || content === "") {
+    alert("right something!");
+    return;
+  }
+  const post = document.createElement("div");
+  post.innerHTML = `
+   <h2>${title}</h2>
+   <p>${content}</p>
+  `;
+  postsContainer.appendChild(post);
+
+  titleInput.value = "";
+  contentInput.value = "";
+});
